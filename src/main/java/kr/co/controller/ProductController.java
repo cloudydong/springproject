f5d643554ce7da1ae98023d1106a1dc62571a4ee
Boseok-Yoo <bossi1@naver.com>
2020-08-06 16:36:41 +0900

    충돌로 합침.

--- src/main/java/kr/co/controller/ProductController.java
+++ src/main/java/kr/co/controller/ProductController.java
@@ -1,13 +1,25 @@
 package kr.co.controller;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.co.domain.ProductDTO;
+import kr.co.domain.ProductImgDTO;
+import kr.co.service.ProductService;
 
 @Controller
 @RequestMapping("product")
 public class ProductController {
+	@Autowired
+	private ProductService productService;
+	
 	@RequestMapping(value = "productList", method = RequestMethod.GET)
 	public void ProductList(String searchValue, Model model) {
 		model.addAttribute("searchValue", searchValue);
@@ -17,4 +29,30 @@ public class ProductController {
 	public void ProductInfo(String ProductCode, Model model) {
 		model.addAttribute("ProductCode", ProductCode);
 	}
+	
+	@RequestMapping(value = "/select/{p_no}", method = RequestMethod.GET)
+	public String selectOne(Model model, @PathVariable Integer p_no) {
+		ProductDTO product = productService.selectByPno(p_no);
+		List<ProductImgDTO> imgList = productService.selectListByPno(p_no);
+		model.addAttribute("product", product);
+		model.addAttribute("imgList", imgList);
+		return "/product/select";
+	}
+	
+	@RequestMapping(value = "/select", method = RequestMethod.GET)
+	public String selectOne(Model model) {
+		return "/product/select";
+	}
+	
+	@RequestMapping(value = "insert", method = RequestMethod.POST)
+	public String insert(HashMap<String, Object> map) {
+		//get List<Img> get Product
+		Integer p_no = productService.insert(map);
+		return "/product/productList";
+	}
+	
+	@RequestMapping(value = "insert", method = RequestMethod.GET)
+	public void insert() {
+	}
+	
 }
